bef82650985a2c98531b77ca316c015fffe5e248
Khushi Patel <khushi@EVISION.COM>
2025-03-03 19:01:51 +0530

    Your commit message

--- JS/tablefilter.js
+++ JS/tablefilter.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
     $('#tableSearch').on('keyup', function () {
         var value = $(this).val().toLowerCase();
-        $('#myTable tbody tr').filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        var found = false;
+
+        $('#myTable tbody tr').each(function () {
+            var rowText = $(this).text().toLowerCase();
+            var isVisible = rowText.indexOf(value) > -1;
+            $(this).toggle(isVisible);
+
+            if (isVisible) found = true;
         });
+
+        $('#notFound').toggle(!found); 
     });
 });
--- JS/weather.js
+++ JS/weather.js
@@ -1,20 +1,12 @@
-$(document).ready(function () {
-    const apiKey = `http://api.openweathermap.org/data/3.0/measurements`;  // Replace with your OpenWeatherMap API key
+$(function () {
+    $("#main").click(function () {
+        var city = $('#city').val();
+        var url = `https://wttr.in/${city}?format=%c+%t+%w+%ph+%+%r+%m+%v+`;
 
-    $('#getWeather').on('click', function () {
-        let city = $('#city').val();
+        $("#info").html('<p class="loading">Loading...</p>');
 
-        if (city) {
-            $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`, function (data) {
-                $('#weather').html(`
-                    <p>Weather in ${data.name}: ${data.main.temp}°C</p>
-                    <p>${data.weather[0].description}</p>
-                `);
-            }).fail(function () {
-                $('#weather').html('<p class="error">City not found. Please try again.</p>');
-            });
-        } else {
-            $('#weather').html('<p class="error">Please enter a city name.</p>');
-        }
+        $.get(url)
+            .done(data => $("#info").html(`<h2>Weather in ${city}</h2><pre>${data}</pre>`))
+            .fail(() => $("#info").html('<p class="error">Data not found</p>'));
     });
 });
